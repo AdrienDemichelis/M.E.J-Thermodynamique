c38dacbb2b5cf24d55399094f88252453b96200a
SvenHo34784 <svenalixho@gmail.com>
2026-03-24 22:44:50 +0200

    Add files via upload

--- testgraph.cpp
+++ testgraph.cpp
@@ -27,11 +27,32 @@ void testgraph() {
     }  */
 
 
-    while (h2->GetBinContent(get<0>(centre), get<1>(centre)) < 1) {
-        double x = r->Gaus(0, 1.5); // moyenne 0, ecart type 1.5
-        double y = r->Gaus(0, 1.5); // moyenne 0, ecart type 1.5
-        h2->Fill(x, y, 0.0001); // incrémente la bin(x, y) de 1 en hauteur ou z
-    }
+
+    auto change_z = [h2](int x, int y) {
+        double z = (h2->GetBinContent(x-1, y) + h2->GetBinContent(x+1, y) + h2->GetBinContent(x, y-1) + h2->GetBinContent(x, y+1)) - 4*h2->GetBinContent(x, y); 
+    
+        h2->SetBinContent(x, y, z);
+        //grille[x][y] = z; // stocke la nouvelle valeur de z dans le tableau 2D
+    };
+
+    
+    h2->SetBinContent(get<0>(centre), get<1>(centre), 1); // met la valeur 1 dans la bin du centre
+
+    double grille[81][81] = {0}; // initialise un tableau 2D de 81x81 avec des zéros
+
+    for (int i = 0; i <= 10; i++) {
+
+        for (int x = 0; x<=get<0>(taille); x++) {
+            for (int y = 0; y<=get<1>(taille); y++) {
+                change_z(x, y);
+                }
+            }
+        }
+
+
+
+
+
 // on peut utiliser Int_t et Double_t c'est pareil que int et double
 
 
