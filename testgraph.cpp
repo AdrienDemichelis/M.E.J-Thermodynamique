75d410087304b3468908eba84505c92213bad0bf
SvenHo34784 <svenalixho@gmail.com>
2026-03-25 14:33:56 +0200

    Add files via upload

--- testgraph.cpp
+++ testgraph.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 void testgraph() {
     TCanvas* c = new TCanvas("c", "TH2D Graph Example", 1200, 800);
-    
-    tuple<int, int> taille(81, 81); // nombre de bins en x et y; NOMBRES IMPAIRS!!!
+    TImage *img = TImage::Create();
+    tuple<int, int> taille(41, 41); // nombre de bins en x et y; NOMBRES IMPAIRS!!!
 
     tuple<int, int> centre((get<0>(taille)/2)+1, (get<1>(taille)/2)+1); //prend la moitie de x et y, et ajoute 1 pour etre au milieu
 
@@ -26,28 +26,32 @@ void testgraph() {
     
     }  */
 
+    double grille[81][81] = {0}; // initialise un tableau 2D de 81x81 avec des zéros
 
 
-    auto change_z = [h2](int x, int y) {
-        double z = (h2->GetBinContent(x-1, y) + h2->GetBinContent(x+1, y) + h2->GetBinContent(x, y-1) + h2->GetBinContent(x, y+1)) - 4*h2->GetBinContent(x, y); 
+    auto change_z = [&](int x, int y) {
+        if ()
+        double z =  (grille[x-1][y] + grille[x+1][y] + grille[x][y-1] + grille[x][y+1]) - 4*grille[x][y];
     
         h2->SetBinContent(x, y, z);
-        //grille[x][y] = z; // stocke la nouvelle valeur de z dans le tableau 2D
+        grille[x][y] = z; // stocke la nouvelle valeur de z dans le tableau 2D
     };
 
     
-    h2->SetBinContent(get<0>(centre), get<1>(centre), 1); // met la valeur 1 dans la bin du centre
-
-    double grille[81][81] = {0}; // initialise un tableau 2D de 81x81 avec des zéros
+    h2->SetBinContent(21, 21, 50);
 
     for (int i = 0; i <= 10; i++) {
 
         for (int x = 0; x<=get<0>(taille); x++) {
             for (int y = 0; y<=get<1>(taille); y++) {
                 change_z(x, y);
+
                 }
             }
-        }
+
+        h2->Draw("LEGO");
+        c->Update();
+    }
 
 
 
@@ -69,7 +73,7 @@ void testgraph() {
     } */ 
 
 
-    //h2->SetBinContent(get<0>(centre), get<0>(centre), 25);
+
 
 /*     int global = h2->FindBin(10, 10);
     h2->AddBinContent(10, 10, 15);
@@ -80,5 +84,5 @@ void testgraph() {
 
     // 4. Draw the histogram
     // "COLZ" option draws a color plot with a color palette on the right side
-    h2->Draw("LEGO");
+
 }
