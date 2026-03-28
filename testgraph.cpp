22ad55888f8c77a1586d41f1839a53f1c6853856
SvenHo34784 <svenalixho@gmail.com>
2026-03-28 19:46:06 +0200

    Add files via upload

--- testgraph.cpp
+++ testgraph.cpp
@@ -19,28 +19,46 @@ void testgraph() {
     // Arguments: name, title (with axis titles), nbinsX, xmin, xmax, nbinsY, ymin, ymax
     TH2D *h2 = new TH2D("h2", "Example TH2D Graph;X Axis [units];Y Axis [units];Z Axis [counts]", get<0>(taille), -4, 4, get<1>(taille), -4, 4);
 
+    double lowerLimit = 1e-4; // valeur minimale de l'axe z
 
-
-    h2->SetBinContent(get<0>(taille)/2 + 1, get<1>(taille)/2 + 1, 1); // initialise la valeur du centre à 1
+    h2->SetMaximum(1); // fixe la valeur maximale de l'axe z à 1 
+    h2->SetMinimum(lowerLimit); // fixe la valeur minimale de l'axe z
+    h2->SetBinContent(get<0>(taille)/2 + 1, get<1>(taille)/2 + 1, 1e-4); // initialise la valeur du centre à 1
     
     double grille[81][81] = {}; //tableau des valeurs de h2 au temps t-1, h2 étant au temps t
 
-
-
-    for (int i = 0; i <= 1000; i++) { // nombre d'itérations pour faire évoluer h2
-        
-        //mettre à jour la grille avec les valeurs de h2
-        for (int x = 0; x < get<0>(taille); x++) {
-            for (int y = 0; y < get<1>(taille); y++) {
-                grille[x][y] = h2->GetBinContent(x, y);
-        }
+    while (h2->GetBinContent(get<0>(taille)/2 + 1, get<1>(taille)/2 + 1) < 1) {
+        h2->SetBinContent(get<0>(taille)/2 + 1, get<1>(taille)/2 + 1, h2->GetBinContent(get<0>(taille)/2 + 1, get<1>(taille)/2 + 1 ) * 2); // initialise la valeur du centre à 1 dans la grille aussi (temps t-1)
+        h2->Draw("LEGO");
+        gPad->SetLogz(); // Met l'échelle de l'axe z en échelle logarithmique
+        c->Update(); // Met à jour le canvas pour afficher les
+        this_thread::sleep_for(chrono::milliseconds(35)); // pause de 100 ms entre chaque mise à jour pour voir l'évolution
     }
 
+    
+    this_thread::sleep_for(chrono::milliseconds(65)); // pause de 3 secondes avant de commencer l'évolution pour voir le point de départ
+    
+    grille[get<0>(taille)/2 + 1][get<1>(taille)/2 + 1] = 1; // initialise la valeur du centre à 1 dans la grille aussi (temps t-1)
+    h2->SetBinContent(get<0>(taille)/2 + 1, get<1>(taille)/2 + 1, 1); // initialise la valeur du centre à 1 dans h2 aussi (temps t)
+
+    double diffusionCoefficient = 0.9; // coefficient de diffusion pour faire évoluer h2
+
+    //for (int i = 0; i <= 1000; i++) { // nombre d'itérations pour faire évoluer h2
+    while (grille[get<0>(taille)/2 + 1][get<1>(taille)/2 + 1] > lowerLimit*2) { // tant que la valeur du centre est supérieure à la valeur minimale de l'axe z    
+        
+        //mettre à jour la grille avec les valeurs de h2
+        for (int x = 0; x <= get<0>(taille); x++) {
+            for (int y = 0; y <= get<1>(taille); y++) {
+                grille[x][y] = h2->GetBinContent(x, y);
+            }
+        }
+
         // Mettre à jour les valeurs de h2 en fonction de la moyenne des voisins dans la grille (d'avant, au temps t-1)
-        for (int x = 1; x <=get<0>(taille); x++) {  
-            for (int y = 1; y <= get<1>(taille); y++) {
-                if (x != get<0>(taille)/2 + 1 || y != get<1>(taille)/2 + 1) {// ne pas mettre à jour la valeur du centre
-                    h2->SetBinContent(x, y, ((grille[x-1][y] + grille[x+1][y] + grille[x][y-1] + grille[x][y+1]) / 4.0));
+        for (int x = 1; x < get<0>(taille); x++) {  
+            for (int y = 1; y < get<1>(taille); y++) {
+                h2->SetBinContent(x, y, grille[x][y] + diffusionCoefficient * ((grille[x-1][y] + grille[x+1][y] + grille[x][y-1] + grille[x][y+1]) / 4.0 - grille[x][y])); // relaxation vers la moyenne des voisins
+                if (h2->GetBinContent(x, y) < lowerLimit) { // si la valeur de h2 est inférieure à la valeur minimale de l'axe z, la fixer à cette valeur minimale
+                    h2->SetBinContent(x, y, lowerLimit);
                 }
             }
         }
@@ -49,11 +67,12 @@ void testgraph() {
 
 
         h2->Draw("LEGO");
+        gPad->SetLogz(); // Met l'échelle de l'axe z en échelle logarithmique
         c->Update(); // Met à jour le canvas pour afficher les changements
         //c->SaveAs(Form("frame_%02d.png", i)); // Sauvegarde chaque frame
+    //}
+
     }
-
-
     
     
     
